8baa605f35ea15a7917659cbd465c20690baae25
Santaigo Gonzalez <santiago.gonzalez@netwconsulting.com>
2017-02-06 16:02:43 -0500

    Initial commit

--- js/loaders.js
+++ js/loaders.js
@@ -1,54 +1,53 @@
 function getAllPages(urlPrefix, callback, page, results) {
-  page = page || 1;
-  results = results || [];
+    page = page || 1;
+    results = results || [];
 
-  var url = urlPrefix + '?per_page=100&page=' + parseInt(page);
+    var url = urlPrefix + '?per_page=100&page=' + parseInt(page);
 
-  $.get(url, function(data) {
-    if (data.length > 0) {
-      data.forEach(function(resultDatum) {
-        results.push(resultDatum);
-      });
-      getAllPages(urlPrefix, callback, page + 1, results);
-    }
-    else {
-      callback(results);
-    }
-  });
+    $.get(url, function(data) {
+        if (data.length > 0) {
+            data.forEach(function(resultDatum) {
+                results.push(resultDatum);
+            });
+            getAllPages(urlPrefix, callback, page + 1, results);
+        } else {
+            callback(results);
+        }
+    });
 }
 
 function getGithubRepos(callback, page, repos) {
-  getAllPages('https://api.github.com/users/yelp/repos', callback);
+    getAllPages('https://api.github.com/users/locoalien/repos', callback);
 }
 
 function getGithubMembers(callback) {
-  getAllPages('https://api.github.com/orgs/yelp/members', callback);
+    getAllPages('https://api.github.com/orgs/locoalien/members', callback);
 }
 
 function loadRepositoryData(repoData) {
-  var org = new Organization('yelp');
-  org.repos = [];
+    var org = new Organization('yelp');
+    org.repos = [];
 
-  repoData.forEach(function(repoDatum) {
-    org.repos.push(new Repository(repoDatum));
-  });
+    repoData.forEach(function(repoDatum) {
+        org.repos.push(new Repository(repoDatum));
+    });
 
-  $('.projects .featured').empty();
-  $('.projects .not-featured').empty();
+    $('.projects .featured').empty();
+    $('.projects .not-featured').empty();
 
-  org.addReposToContainer($('.projects .featured'), org.featuredRepos());
-  org.addReposToContainer($('.projects .not-featured'), org.regularRepos());
+    org.addReposToContainer($('.projects .featured'), org.featuredRepos());
+    org.addReposToContainer($('.projects .not-featured'), org.regularRepos());
 
-  $('.project-count').html(org.forkedCount());
+    $('.project-count').html(org.forkedCount());
 }
 
 function loadMemberData(members) {
-  $('.dev-count').html(members.length);
+    $('.dev-count').html(members.length);
 }
 
 $(document).ready(function() {
-  getGithubRepos(loadRepositoryData);
-  getGithubMembers(loadMemberData);
+    getGithubRepos(loadRepositoryData);
+    getGithubMembers(loadMemberData);
 });
 
 
